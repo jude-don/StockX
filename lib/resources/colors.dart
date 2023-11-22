8081ea6d9e771a6f40f10ad09109f38bc6376a58
[jude-don] <judedonkor75@gmail.com>
2023-11-22 14:08:48 -0500

    Initial commit

---
+++ lib/resources/colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  AppColors._();
+
+  static const primaryColor = Color(0xFF292541);
+  static const primaryVariantColor = Color(0xFF676586);
+  static const secondaryColor = Color(0xFFE66239);
+  static const surfaceColor = Color(0xFFFFFFFF);
+  static const borderColor = Color(0x9CE2E2E2);
+  static const premiumColor = Color(0xFFBB9D05);
+  static const appRedColor = Color(0xFFE20000);
+  static const graphColor1 = Color(0xFF00C893);
+  static const graphColor2 = Color(0xFFFEDFF0);
+  static const graphColor3 = Color(0xFF676585);
+  static const gradient1 = Color(0xFFEAEAEA);
+  static const gradient2 = Color(0xFFFFFFFF);
+
+}
